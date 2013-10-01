7e375f54e7f2988684a18ed553740d58722136ff
healqq <nyashin92@gmail.com>
2013-10-01 17:29:34 +0400

    soap request save script +
    конструктор запросов, запрос web - > 1C для сохранения сценария.

--- nick_tests_jq/js/Ajax.js
+++ nick_tests_jq/js/Ajax.js
@@ -1,81 +1,58 @@
- // Загружает некоторые данные на сервер и оповещает пользователя по окончанию операции.
- 
- $.ajax({
-   type: "POST",
-   url: "some.php",
-   data: "name=John&location=Boston",
-   success: function(msg){
-     alert( "Data Saved: " + msg );
-   }
- });
- 
- 
- // Отсылает документ XML в качестве данных на сервер. Автоматическое преобразование данных запрещено путем установки опции processData в false.
-
- var xmlDocument = [create xml document];
- $.ajax({
-   type: "GET",
-   url: "page.php",
-   processData: true,
-   data: xmlDocument,
-   success: handleResponse
- });
-
-    Код
-
-// Посылает идентификатор в качестве данных на сервер, сохраняет данные и оповещает пользователя по окончанию операции.
-
- bodyContent = $.ajax({
-      url: "script.php",
-      global: false,
-      type: "POST",
-      data: ({id : this.getAttribute('id')}),
-      dataType: "html",
-      success: function(msg){
-         alert(msg);
-      }
-   }
-).responseText;
-
-
-// function getStr(form)
-// {
-	// var tmp = [], el;
-	// for(i=0; el = form.elements[i]; i++)
-		// if( el.type == "hidden" && el.value != "" ) tmp.push(el.name + '=' + el.value);
-	
-// }
-
-
-
-
-       function OtpravkaXML() {
-            $("#btnCallWebService").click(function (event) {
-                var wsUrl = "http://www.cbr.ru/dailyinfowebserv/dailyinfo.asmx?WSDL";
-                var soapRequest ='<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">   <soap:Body> <getQuote xmlns:impl="http://abc.com/services/soap/server1.php">  <symbol>' + $("#txtName").val() + '</symbol>   </getQuote> </soap:Body></soap:Envelope>';
-                               alert(soapRequest)
-                $.ajax({
+function saveStructure() {
+    
+    var wsUrl = 'ws/ws/callcenterexchange';
+    var soapRequest = combineSoapRequest("SetScriptStructure",[{key:'EncodedData', value: exportToJSON("#container")}]) ;
+    alert(soapRequest);
+    var request = $.ajax({
                     type: "POST",
                     url: wsUrl,
                     contentType: "text/xml",
-                    dataType: "xml",
-                    data: soapRequest,
-                    success: processSuccess,
-                    error: processError
+                    dataType: "html",
+                    data: soapRequest
                 });
+	request.done(function( msg ) {
+		$( "#response" ).html( msg );
+		});
+	request.fail(function( jqXHR, textStatus ) {
+		alert( "Request failed: " + textStatus );
+		});
+            
+}
 
-            });
-        });
+function processSuccess(data, status, req) { 
+	alert('success');
+    if (status == "success")
+		$("#response").text($(req.responseXML).find("Result").text());
+    alert(req.responseXML);
+}
 
-        function processSuccess(data, status, req) { alert('success');
-            if (status == "success")
-                $("#response").text($(req.responseXML).find("Result").text());
-
-                alert(req.responseXML);
-        }
-
-        function processError(data, status, req) {
-        alert('err'+data.state);
+function processError(data, status, req) {
+    alert('err'+data.state);
             //alert(req.responseText + " " + status);
-        } 
+} 
+//формирует soap реквест
+function combineSoapRequest(action, arrayParam){
+	var paramString = '';
+	if (! (arrayParam == undefined) ){ 
+		for (var i=0; i< arrayParam.length;i++){
+			paramString += fillParam(arrayParam[i].key, arrayParam[i].value);
+		}
+	}
+
+var request =	'<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ctoc="www.1ctocallcenter.com">\
+				<soapenv:Header/>\
+				<soapenv:Body>\
+				<ctoc:' + action + '>' + paramString + '\
+				</ctoc:' + action + '>\
+				</soapenv:Body>\
+				</soapenv:Envelope>';
+	return request;
+}
+function fillParam(key,value){
+	return '<ctoc:'+key+'>'+value+'</ctoc:'+key+'>';
+}
+ 
+
+
+
 
