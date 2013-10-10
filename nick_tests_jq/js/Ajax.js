f303b6fcbf6720994abc008865175dd0234eebe4
healqq <nyashin92@gmail.com>
2013-10-10 14:31:44 +0400

    авторизацию доделал

--- nick_tests_jq/js/Ajax.js
+++ nick_tests_jq/js/Ajax.js
@@ -4,6 +4,8 @@ function saveStructure() {
     request = sendRequest("SetScriptStructure", [{key:'EncodedData', value: exportToJSON(["#container","#imported"])}]);
 	request.done(function( msg ) {
 		$( "#response" ).html( msg );
+		if ( $("#response").find("m\\:return").attr('xsi:nil') == true )
+			redirect("testauth.html");
 		});
 	request.fail(function( jqXHR, textStatus ) {
 		alert( "Request failed: " + textStatus );
@@ -16,6 +18,10 @@ function loadStructure() {
     request = sendRequest("GetScriptStructure");
 	request.done(function( msg ) {
 			$( "#response" ).html( msg );
+			if ( $.parseJSON($("#response").find("m\\:return").attr('xsi:nil')) == true ){
+				redirect("testauth.html");
+				return;
+				}
 			first = importFromJSON( $( "#response" ).text(), "#imported", false);
 			showBranch(first,false);
 		});
@@ -44,6 +50,14 @@ function sendRequest(action, params){
 //формирует soap реквест
 function combineSoapRequest(action, arrayParam){
 	var paramString = '';
+	//параметр токен обязателен
+	var sessionID = getCookie('PHPSESSID');
+	if (sessionID == undefined){
+		redirect("testauth.html");
+	}
+	//params = [{key:'Token',value:sessionID}];
+	paramString += fillParam('Token', sessionID); 
+	
 	if (! (arrayParam == undefined) ){ 
 		for (var i=0; i< arrayParam.length;i++){
 			paramString += fillParam(arrayParam[i].key, arrayParam[i].value);
--- nick_tests_jq/js/Auth.js
+++ nick_tests_jq/js/Auth.js
@@ -1,3 +1,4 @@
+//устанавливает куку
 function setCookie(name, value, options) {
   options = options || {};
 
@@ -26,7 +27,7 @@ function setCookie(name, value, options) {
 
   document.cookie = updatedCookie;
 }
-
+//получает куку по имени или undefined
 function getCookie(name) {
 
   var matches = document.cookie.match(new RegExp(
@@ -35,43 +36,45 @@ function getCookie(name) {
   return matches ? decodeURIComponent(matches[1]) : undefined;
 
 }
-
+//очищает куку по имени
 function removeCookie(name){
 	setCookie(name, "deleted", {expires:-24*60*60,path:'/'});
 }
-
+//проверяет наличие сессии в куках
 function checkSession(){
 	var cookieSESSID = getCookie('PHPSESSID');
 	if (cookieSESSID == undefined) {
-		//show login
+		//show login page
 		redirect("testauth.html");
 		
 		//auth(login, pwd);
-		//start new session ( send request login, pwd
+		//start new session ( send request login, pwd )
 	}
 	else{
 		confimSession(cookieSESSID);
 		//send request with sessid
 	}
 }
-
+//при нажатии кнопки логин
 function login(){
 	login = $("#login").val();
 	pwd	  = $("#pwd").val();
 	var login_pattern = /^[a-zA-Z0-9]{3,30}$/;
 	var pwd_pattern = /^[a-zA-Z0-9]{6,30}$/;
+	//проверка логина и пароля регуляркой
 	if (login_pattern.test(login) && pwd_pattern.test(pwd) ){
 	//	alert('all ok');
 		auth(login, pwd);
 	}
 	else{
 		alert(' Неверная пара логин пароль!');
-		return false;
+		//return false;
 	}
 	
 }
 function auth(login, pwd){
 	params = [{key:'Login',value:login},{key:'Pwd',value:pwd}]; 
+	//тут надо добавить кодирование пароля
 	request = sendRequest("auth",params);
 	request.done(function (msg){
 		$( "#response" ).html( msg );
@@ -92,23 +95,32 @@ function auth(login, pwd){
 		alert( "Не удалось выполнить запрос к серверу по причине: " + textStatus );
 		});
 }
+//проверка, что текущая сессия существует на сервере
 function confimSession(sessionID){
-	params = [{key:'Token',value:sessionID}];
-	request = sendRequest("CheckSession",params);
+	//params = [{key:'Token',value:sessionID}];
+	request = sendRequest("CheckSession");
 	request.done(function (msg){
 		$( "#response" ).html( msg );
-		SESSID =  $.trim($( "#response" ).text());
+		reqAttr = $("#response").find("m\\:return").attr('xsi:nil');
+		if (!(reqAttr == undefined) && $.parseJSON(reqAttr) == true ){
+				removeCookie('PHPSESSID');
+				redirect("testauth.html");
+				return;
+			}		
+		sessionType =  $.trim($( "#response" ).text());
 		//alert(SESSID);
-		if ( $.parseJSON(SESSID) ) {
+		if ( sessionType == "admin" ) {
 			//all ok
 			setCookie('PHPSESSID', sessionID,{expires:24*60, path:'/'});
 			redirect("index.html");
 			//return true;
 		}
 		else{
+		
+		
 			//alert("Неправильная пара логин пароль!");
-			removeCookie('PHPSESSID');
-			redirect("testauth.html");
+			setCookie('PHPSESSID', sessionID,{expires:24*60, path:'/'});
+			redirect("client.html");
 			//window.location.replace("testauth.html");
 			//return false;
 		}
@@ -120,7 +132,10 @@ function confimSession(sessionID){
 	
 }
 function redirect(path){
-	var basePath = "http://localhost/Callcenter/";
+	var findString = '/Callcenter/';
+	var href  	   = String(window.location.href.toString());
+	index = href.search('/Callcenter/');
+	basePath = href.substring(0,index + findString.length);
 	if (!(window.location.href == basePath+path)){
 			window.location.replace(path);
 	
@@ -128,9 +143,9 @@ function redirect(path){
 }
 
 function logout(){
-	var sessionID = getCookie('PHPSESSID');
-	params = [{key:'Token',value:sessionID}];
-	request = sendRequest("Logout",params);
+	//var sessionID = getCookie('PHPSESSID');
+	//params = [{key:'Token',value:sessionID}];
+	request = sendRequest("Logout");
 	request.done(function (msg){
 			removeCookie('PHPSESSID');
 			redirect("testauth.html");
