bae0220305cc45ed1d776567a35773a7f954a8f5
healqq <nyashin92@gmail.com>
2013-11-28 12:23:05 +0400

    tree view changes
    переместил блок с информацией вниз схемы дерева

--- nick_tests_jq/js/drawing.js
+++ nick_tests_jq/js/drawing.js
@@ -14,6 +14,7 @@ var drawerSingleton = (function(){
 	var blockHeight = 40;
 	var spaceCoff = 0.5;
 	var blockSize = blockWidth *(1 +  spaceCoff);
+	var initialShift = 0.5*blockHeight;
 	
 	//draw element 
 	var drawNext = ( function( element, row, column){
@@ -96,7 +97,7 @@ var drawerSingleton = (function(){
 	var canvasElement 	= $(canvas);
 	var oldCanvas = undefined;
 		var resizeWidth =  ( (parseInt( canvasElement.css('width') ) - rows * blockSize ) < blockSize ) ;
-		var resizeHeight = ( (parseInt( canvasElement.css('height') ) - columns * blockSize ) < blockSize ) ; 
+		var resizeHeight = ( (parseInt( canvasElement.css('height') ) - columns * blockSize - initialShift ) < blockSize ) ; 
 		var widthBefore, widthAfter, heightBefore, heightAfter;
 		if  ( resizeWidth || resizeHeight ){
 			//resizing
@@ -150,7 +151,7 @@ var drawerSingleton = (function(){
 					ctx.beginPath();
 
 					ctx.arc(  ( 0.5 + (options.row) * (1+options.spaceCoff) ) * options.blockWidth,
-						(0.5 + options.column*(1+options.spaceCoff) )*options.blockHeight, 
+						(0.5 + options.column*(1+options.spaceCoff) )*options.blockHeight + initialShift, 
 						options.spaceCoff * options.blockWidth,0,
 						2*Math.PI);
 					ctx.fill();
@@ -159,7 +160,7 @@ var drawerSingleton = (function(){
 				ctx.beginPath();
 				ctx.lineWidth = 2;
 				ctx.arc(  ( 0.5 + (options.row) * (1+options.spaceCoff) ) * options.blockWidth,
-					(0.5 + options.column*(1+options.spaceCoff) )*options.blockHeight, 
+					(0.5 + options.column*(1+options.spaceCoff) )*options.blockHeight + initialShift, 
 					options.spaceCoff * options.blockWidth,0,
 					2*Math.PI);
 				ctx.stroke();
@@ -172,7 +173,7 @@ var drawerSingleton = (function(){
 			//	var yShift = 0.6;
 				ctx.fillText(''+options.branchesSize,
 					(0.5 + (options.row)  * (1+options.spaceCoff)) * options.blockWidth,
-					(0.5  +  options.column*(1+options.spaceCoff) )*options.blockHeight);
+					(0.5  +  options.column*(1+options.spaceCoff) )*options.blockHeight + initialShift);
 			break;
 			case "square":
 				//ctx.fillStyle = "blue";
@@ -191,13 +192,13 @@ var drawerSingleton = (function(){
 				if (options.currentElement){
 					ctx.fillStyle = grd;
 					ctx.fillRect((options.row)*(1+options.spaceCoff)*options.blockWidth,
-						options.column*(1+options.spaceCoff)*options.blockHeight,
+						options.column*(1+options.spaceCoff)*options.blockHeight + initialShift,
 						options.blockWidth,
 						options.blockHeight);	
 				}
 				ctx.lineWidth = 2;
 				ctx.strokeRect((options.row)*(1+options.spaceCoff)*options.blockWidth,
-					options.column*(1+options.spaceCoff)*options.blockHeight,
+					options.column*(1+options.spaceCoff)*options.blockHeight + initialShift,
 					options.blockWidth,
 					options.blockHeight);
 			break;
@@ -207,9 +208,9 @@ var drawerSingleton = (function(){
 				ctx.beginPath();
 				
 				var startX  = (options.row*(1+options.spaceCoff) + options.spaceCoff )*options.blockWidth + 0.5;
-				var startY  = (1 + options.column*(1+options.spaceCoff) )*options.blockHeight;
+				var startY  = (1 + options.column*(1+options.spaceCoff) )*options.blockHeight + initialShift;
 				var finishX = ( ( options.rowComputed )*(1+options.spaceCoff) + options.spaceCoff )*options.blockWidth + 0.5;
-				var finishY = ((options.column+1)*(1+options.spaceCoff) )*options.blockHeight;
+				var finishY = ((options.column+1)*(1+options.spaceCoff) )*options.blockHeight + initialShift;
 				ctx.moveTo( startX  ,startY );
 				if (options.row == options.rowComputed){
 					ctx.lineTo( finishX, finishY);
@@ -236,7 +237,7 @@ var drawerSingleton = (function(){
 	//ищет блок по координатам 
 	var findBlock = ( function( pageX, pageY){
 		var mouseX 	= pageX - canvas.offsetLeft;
-		var mouseY 	= pageY - canvas.offsetTop;
+		var mouseY 	= pageY - canvas.offsetTop - initialShift;
 		var retValue = {rows:undefined, columns: undefined, found: true};
 		var rows 	= (mouseX  ) /( blockWidth*(1+spaceCoff) );
 		var columns = (mouseY  ) /( blockHeight*(1+spaceCoff) );
@@ -366,7 +367,7 @@ var drawerSingleton = (function(){
 		//layer =( layer == undefined? 1, layer);
 			element = $("#container").children().first().attr('id');
 			//ctx.scale(0.25, 0.25);
-			drawNext(element, 1, 1);
+			drawNext(element, 1, 0);
 			//ctx.scale(0.5, 0.5);
 			
 			//do stuff
@@ -378,7 +379,7 @@ var drawerSingleton = (function(){
 				itemsArray = []; 
 				ctx = canvas.getContext('2d');
 				maxRow = 0;
-				maxColumn = 1;
+				maxColumn = 0;
 				canvas.height = 240;
 				canvas.width = 240;
 				canvas.addEventListener("click",mouseclick);
